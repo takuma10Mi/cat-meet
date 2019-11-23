56a27ee0e614587f4a9046af51c32746e7cd2fe1
takuma10Mi <takumaii@yahoo.co.jp>
2019-11-23 11:14:57 +0900

    history 仮

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,8 +6,20 @@ $(function() {
     //マウスを乗せたら色が変わる
     $(this).css('font-size', '50px');
 
-
   }, function(){
     $(this).css('font-size', '');
   });
+
+  // #で始まるリンクをクリックしたら実行されます
+  $('a[href^="#"]').click(function() {
+    // スクロールの速度
+    var headerHight = 100;
+    var speed = 400; // ミリ秒で記述
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+
 });
